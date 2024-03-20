5c16cb9ce8c68dd890373a11d891737856550ade
henrytracey <116266179+creamedbeans@users.noreply.github.com>
2024-03-20 14:11:15 +0000

    Add files via upload
    base files

---
+++ MusicManagerApp/src/main/java/com/mycompany/musicmanagerapp/MusicManagerApp.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.musicmanagerapp;
+
+/**
+ *
+ * @author henry
+ */
+public class MusicManagerApp {
+
+    public static void main(String[] args) {
+    }
+}
